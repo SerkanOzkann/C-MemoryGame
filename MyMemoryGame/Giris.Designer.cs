fc47cc77fbfc1478f24e6e2f1b33cc32ec6c6773
Serkan ÖZKAN <57104731+Delete1903@users.noreply.github.com>
2021-05-22 20:53:00 +0300

    Add project files.

---
+++ MyMemoryGame/Giris.Designer.cs
@@ -0,0 +1,299 @@
+﻿namespace MyMemoryGame
+{
+    partial class Giris
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Giris));
+            this.pb_m = new System.Windows.Forms.PictureBox();
+            this.pb_y = new System.Windows.Forms.PictureBox();
+            this.pb_m1 = new System.Windows.Forms.PictureBox();
+            this.pb_e = new System.Windows.Forms.PictureBox();
+            this.pb_m3 = new System.Windows.Forms.PictureBox();
+            this.pb_o = new System.Windows.Forms.PictureBox();
+            this.pb_r = new System.Windows.Forms.PictureBox();
+            this.pb_y2 = new System.Windows.Forms.PictureBox();
+            this.gb_giris = new System.Windows.Forms.GroupBox();
+            this.pb_user = new System.Windows.Forms.PictureBox();
+            this.tb_kadi = new System.Windows.Forms.TextBox();
+            this.btn_kolay = new System.Windows.Forms.Button();
+            this.btn_orta = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_m)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_y)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_m1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_e)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_m3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_o)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_r)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_y2)).BeginInit();
+            this.gb_giris.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_user)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pb_m
+            // 
+            this.pb_m.BackColor = System.Drawing.Color.Transparent;
+            this.pb_m.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_m.Image = global::MyMemoryGame.Properties.Resources.keyboard_key_m;
+            this.pb_m.Location = new System.Drawing.Point(312, 39);
+            this.pb_m.Name = "pb_m";
+            this.pb_m.Size = new System.Drawing.Size(66, 50);
+            this.pb_m.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_m.TabIndex = 0;
+            this.pb_m.TabStop = false;
+            // 
+            // pb_y
+            // 
+            this.pb_y.BackColor = System.Drawing.Color.Transparent;
+            this.pb_y.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_y.Image = global::MyMemoryGame.Properties.Resources.y;
+            this.pb_y.Location = new System.Drawing.Point(384, 39);
+            this.pb_y.Name = "pb_y";
+            this.pb_y.Size = new System.Drawing.Size(66, 50);
+            this.pb_y.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_y.TabIndex = 1;
+            this.pb_y.TabStop = false;
+            // 
+            // pb_m1
+            // 
+            this.pb_m1.BackColor = System.Drawing.Color.Transparent;
+            this.pb_m1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_m1.Image = global::MyMemoryGame.Properties.Resources.keyboard_key_m;
+            this.pb_m1.Location = new System.Drawing.Point(168, 95);
+            this.pb_m1.Name = "pb_m1";
+            this.pb_m1.Size = new System.Drawing.Size(66, 50);
+            this.pb_m1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_m1.TabIndex = 2;
+            this.pb_m1.TabStop = false;
+            // 
+            // pb_e
+            // 
+            this.pb_e.BackColor = System.Drawing.Color.Transparent;
+            this.pb_e.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_e.Image = global::MyMemoryGame.Properties.Resources.keyboard_key_e;
+            this.pb_e.Location = new System.Drawing.Point(240, 95);
+            this.pb_e.Name = "pb_e";
+            this.pb_e.Size = new System.Drawing.Size(66, 50);
+            this.pb_e.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_e.TabIndex = 3;
+            this.pb_e.TabStop = false;
+            // 
+            // pb_m3
+            // 
+            this.pb_m3.BackColor = System.Drawing.Color.Transparent;
+            this.pb_m3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_m3.Image = global::MyMemoryGame.Properties.Resources.keyboard_key_m;
+            this.pb_m3.Location = new System.Drawing.Point(312, 95);
+            this.pb_m3.Name = "pb_m3";
+            this.pb_m3.Size = new System.Drawing.Size(66, 50);
+            this.pb_m3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_m3.TabIndex = 4;
+            this.pb_m3.TabStop = false;
+            // 
+            // pb_o
+            // 
+            this.pb_o.BackColor = System.Drawing.Color.Transparent;
+            this.pb_o.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_o.Image = global::MyMemoryGame.Properties.Resources.key_o_of_a_compter_keyboard;
+            this.pb_o.Location = new System.Drawing.Point(384, 95);
+            this.pb_o.Name = "pb_o";
+            this.pb_o.Size = new System.Drawing.Size(66, 50);
+            this.pb_o.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_o.TabIndex = 5;
+            this.pb_o.TabStop = false;
+            // 
+            // pb_r
+            // 
+            this.pb_r.BackColor = System.Drawing.Color.Transparent;
+            this.pb_r.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_r.Image = global::MyMemoryGame.Properties.Resources.letter_r_key_on_keyboard;
+            this.pb_r.Location = new System.Drawing.Point(456, 95);
+            this.pb_r.Name = "pb_r";
+            this.pb_r.Size = new System.Drawing.Size(66, 50);
+            this.pb_r.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_r.TabIndex = 6;
+            this.pb_r.TabStop = false;
+            // 
+            // pb_y2
+            // 
+            this.pb_y2.BackColor = System.Drawing.Color.Transparent;
+            this.pb_y2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pb_y2.Image = global::MyMemoryGame.Properties.Resources.y;
+            this.pb_y2.Location = new System.Drawing.Point(528, 95);
+            this.pb_y2.Name = "pb_y2";
+            this.pb_y2.Size = new System.Drawing.Size(66, 50);
+            this.pb_y2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_y2.TabIndex = 7;
+            this.pb_y2.TabStop = false;
+            // 
+            // gb_giris
+            // 
+            this.gb_giris.BackColor = System.Drawing.Color.Transparent;
+            this.gb_giris.Controls.Add(this.button3);
+            this.gb_giris.Controls.Add(this.btn_orta);
+            this.gb_giris.Controls.Add(this.btn_kolay);
+            this.gb_giris.Controls.Add(this.tb_kadi);
+            this.gb_giris.Controls.Add(this.pb_user);
+            this.gb_giris.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.gb_giris.Location = new System.Drawing.Point(215, 164);
+            this.gb_giris.Name = "gb_giris";
+            this.gb_giris.Size = new System.Drawing.Size(304, 293);
+            this.gb_giris.TabIndex = 8;
+            this.gb_giris.TabStop = false;
+            this.gb_giris.Text = "Giriş Yap";
+            // 
+            // pb_user
+            // 
+            this.pb_user.Image = global::MyMemoryGame.Properties.Resources.customer;
+            this.pb_user.Location = new System.Drawing.Point(15, 29);
+            this.pb_user.Name = "pb_user";
+            this.pb_user.Size = new System.Drawing.Size(60, 62);
+            this.pb_user.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pb_user.TabIndex = 0;
+            this.pb_user.TabStop = false;
+            // 
+            // tb_kadi
+            // 
+            this.tb_kadi.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.tb_kadi.Location = new System.Drawing.Point(93, 50);
+            this.tb_kadi.Name = "tb_kadi";
+            this.tb_kadi.Size = new System.Drawing.Size(169, 27);
+            this.tb_kadi.TabIndex = 1;
+            this.tb_kadi.Text = "Kullanıcı Adı Giriniz...";
+            // 
+            // btn_kolay
+            // 
+            this.btn_kolay.BackColor = System.Drawing.Color.Lime;
+            this.btn_kolay.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_kolay.Location = new System.Drawing.Point(113, 107);
+            this.btn_kolay.Name = "btn_kolay";
+            this.btn_kolay.Size = new System.Drawing.Size(97, 43);
+            this.btn_kolay.TabIndex = 2;
+            this.btn_kolay.Text = "KOLAY";
+            this.btn_kolay.UseVisualStyleBackColor = false;
+            this.btn_kolay.Click += new System.EventHandler(this.btn_kolay_Click);
+            // 
+            // btn_orta
+            // 
+            this.btn_orta.BackColor = System.Drawing.Color.DarkOrange;
+            this.btn_orta.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btn_orta.Location = new System.Drawing.Point(113, 166);
+            this.btn_orta.Name = "btn_orta";
+            this.btn_orta.Size = new System.Drawing.Size(97, 43);
+            this.btn_orta.TabIndex = 3;
+            this.btn_orta.Text = "ORTA";
+            this.btn_orta.UseVisualStyleBackColor = false;
+            this.btn_orta.Click += new System.EventHandler(this.btn_orta_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Red;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.Location = new System.Drawing.Point(113, 233);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(97, 43);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "ZOR";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.linkLabel1.ForeColor = System.Drawing.Color.White;
+            this.linkLabel1.LinkColor = System.Drawing.Color.White;
+            this.linkLabel1.Location = new System.Drawing.Point(257, 460);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(220, 22);
+            this.linkLabel1.TabIndex = 9;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "GitHub.com/Serkanozkann";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Giris
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::MyMemoryGame.Properties.Resources.bg3;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(747, 535);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.gb_giris);
+            this.Controls.Add(this.pb_y2);
+            this.Controls.Add(this.pb_r);
+            this.Controls.Add(this.pb_o);
+            this.Controls.Add(this.pb_m3);
+            this.Controls.Add(this.pb_e);
+            this.Controls.Add(this.pb_m1);
+            this.Controls.Add(this.pb_y);
+            this.Controls.Add(this.pb_m);
+            this.Font = new System.Drawing.Font("Times New Roman", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Giris";
+            this.Text = "MyMemory";
+            this.Load += new System.EventHandler(this.Giris_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pb_m)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_y)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_m1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_e)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_m3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_o)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_r)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_y2)).EndInit();
+            this.gb_giris.ResumeLayout(false);
+            this.gb_giris.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_user)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pb_m;
+        private System.Windows.Forms.PictureBox pb_y;
+        private System.Windows.Forms.PictureBox pb_m1;
+        private System.Windows.Forms.PictureBox pb_e;
+        private System.Windows.Forms.PictureBox pb_m3;
+        private System.Windows.Forms.PictureBox pb_o;
+        private System.Windows.Forms.PictureBox pb_r;
+        private System.Windows.Forms.PictureBox pb_y2;
+        private System.Windows.Forms.GroupBox gb_giris;
+        private System.Windows.Forms.PictureBox pb_user;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button btn_orta;
+        private System.Windows.Forms.Button btn_kolay;
+        private System.Windows.Forms.TextBox tb_kadi;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ MyMemoryGame/Giris.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MyMemoryGame
+{
+    public partial class Giris : Form
+    {
+        public Giris()
+        {
+            InitializeComponent();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start("https://github.com/SerkanOzkann");
+        }
+
+        private void Giris_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btn_kolay_Click(object sender, EventArgs e)
+        {
+            SeviyeKolay f1 = new SeviyeKolay();
+
+            if (tb_kadi.Text == "")
+            {
+                MessageBox.Show("Oyuncu adı boş bırakılamaz.");
+            }
+            else
+            {
+                f1.Show();
+                this.Hide();
+            }
+        }
+
+        private void btn_orta_Click(object sender, EventArgs e)
+        {
+            SeviyeOrta f2 = new SeviyeOrta();
+            if (tb_kadi.Text == "")
+            {
+                MessageBox.Show("Oyuncu adı boş bırakılamaz.");
+            }
+            else
+            {
+                f2.Show();
+                this.Hide();
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SeviyeZor f3 = new SeviyeZor();
+
+            if (tb_kadi.Text == "")
+            {
+                MessageBox.Show("Oyuncu adı boş bırakılamaz.");
+            }
+            else
+            {
+                f3.Show();
+                this.Hide();
+            }
+        }
+    }
+}
